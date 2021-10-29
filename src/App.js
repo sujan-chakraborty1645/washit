f45cf876f843a3f49fae7ab470c33e66030bffb0
Sujan <sujan.botxop21i61@gmail.com>
2021-10-29 19:17:25 +0530

    testimonials added perfectly responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import {Our} from './ourservice';
+import { Testimonials } from './testimonials';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Our/>
+      <Testimonials/>
     </div>
   );
 }
---
+++ src/testimonials.js
@@ -0,0 +1,17 @@
+export let Testimonials=()=>{
+    return(
+        <>
+         <div className="testimonials_cover">
+              <h3 className="ourheadline">Testimonials</h3> 
+            <div className="testimonials">
+                   <div className="testimonial_image1"></div>
+                   <div className="testimonial_text">
+                       <div className="testimonial_text_name">John Honai</div>
+                       <div className='testimonial_text_customerinfo'>Premium Customer</div>
+                       <div className='testimonial_text_comment'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas ac mauris ut neque. In dolor felis amet, tortor pulvinar tempor est eu nisl. Eget amet nibh bibendum accumsan. Faucibus at rutrum tempus vestibulum. Id aliquam imperdiet egestas phasellus aenean ipsum condimentum.</div>
+                   </div>
+            </div>
+         </div>
+        </>
+    )
+}
